cc6b766f89eff17c3445a69f5a71565ee9522cbf
glendonchin <glendonchin@gmail.com>
2023-09-22 10:24:45 -0400

    Move character lookup from public folder to CharacterEncyclopedia page and route it

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const PORT = process.env.PORT || 4000;
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
+// Serve static files from the CharacterEncyclopedia directory
+app.use('/CharacterEncyclopedia', express.static(path.join(__dirname, 'src/pages/CharacterEncyclopedia')));
+
+
 // Simple route for the home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/src/pages/CharacterEncyclopedia', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/pages/CharacterEncyclopedia/index.html'));
 });
 
 app.listen(PORT, () => {
