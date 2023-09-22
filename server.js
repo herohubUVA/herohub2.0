7c22e6d39969320bb1dc93b45e1ddbcb6d1cd417
glendonchin <glendonchin@gmail.com>
2023-09-22 10:48:26 -0400

    Redirect default page to Home page

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// Serve static files from the Home directory as the root
+app.use(express.static(path.join(__dirname, 'src/pages/Home')));
 
 // Serve static files from the CharacterEncyclopedia directory
 app.use('/CharacterEncyclopedia', express.static(path.join(__dirname, 'src/pages/CharacterEncyclopedia')));
 
+// Serve other static files from the "public" directory
+app.use(express.static('public'));
 
-// Simple route for the home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/src/pages/CharacterEncyclopedia', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/pages/CharacterEncyclopedia/index.html'));
+// Route for the CharacterEncyclopedia (this may be redundant since the static middleware above might handle this)
+app.get('/CharacterEncyclopedia', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/pages/CharacterEncyclopedia/index.html'));
 });
 
 app.listen(PORT, () => {
