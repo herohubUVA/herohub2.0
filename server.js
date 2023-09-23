4b56f2e42e64ca5d34ea4ebd06ecfbb25d98c7db
glendonchin <glendonchin@gmail.com>
2023-09-23 14:59:46 -0400

    Fix server order structure

--- server.js
+++ server.js
@@ -6,17 +6,13 @@ const PORT = process.env.PORT || 4000;
 // Serve static files from the Home directory as the root
 app.use(express.static(path.join(__dirname, 'src/pages/Home')));
 
+// Serve other static files from the "public" directory
+app.use(express.static('public'));
 
 // Serve static files from the CharacterEncyclopedia directory
 app.use('/CharacterEncyclopedia', express.static(path.join(__dirname, 'src/pages/CharacterEncyclopedia')));
 
-// Serve other static files from the "public" directory
-app.use(express.static('public'));
 
-// Route to Character Encyclopedia (from navigation bar)
-app.get('/CharacterEncyclopedia', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/pages/CharacterEncyclopedia/index.html'));
-});
 
 // Route for the CharacterEncyclopedia (this may be redundant since the static middleware above might handle this)
 app.get('/CharacterEncyclopedia', (req, res) => {
