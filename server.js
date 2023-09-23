94e0481caaf76ab86ebdf31101c7f54fe2cd69b4
glendonchin <glendonchin@gmail.com>
2023-09-23 16:35:01 -0400

    Reorder middleware to show functionality

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
+// Import required modules
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const serveIndex = require('serve-index');
-
-app.use(express.static(__dirname+'/src'));
+// Middleware to serve assets directory
 app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
+
+// Middleware to serve static files from 'src/pages/Home' directory
 app.use(express.static(path.resolve(__dirname, 'src/pages/Home')));
+
+// Middleware to serve static files from 'public' directory
 app.use(express.static('public'));
+
+// Middleware to serve static files from 'src/pages/CharacterEncyclopedia' directory
 app.use('/CharacterEncyclopedia', express.static(path.resolve(__dirname, 'src/pages/CharacterEncyclopedia')));
 
+// Middleware to serve static files from 'src' directory
+app.use(express.static(__dirname + '/src'));
 
-
-// Print port that the server is running on
+// Start the server and print the port it's running on
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
