0a4d8ee308ad1bd285f3394d895f8e43afc56d22
glendonchin <glendonchin@gmail.com>
2023-09-25 09:38:16 -0400

    Fix audio theme switch and add alt audio for dark

--- src/pages/Home/app.js
+++ src/pages/Home/app.js
@@ -1,86 +1,97 @@
-// Select The Elements
 var toggle_btn;
 var big_wrapper;
 var hamburger_menu;
-
-function declare() {
-  toggle_btn = document.querySelector(".toggle-btn");
-  big_wrapper = document.querySelector(".big-wrapper");
-  hamburger_menu = document.querySelector(".hamburger-menu");
-}
-
 const main = document.querySelector("main");
-
-declare();
+let playButton;
+const audio = new Audio('/assets/audio/marvelOpening.mp3');
+let volumeBars;
 
 let dark = false;
 
+function declare() {
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+    hamburger_menu = document.querySelector(".hamburger-menu");
+    playButton = document.querySelector('.play-button');
+    volumeBars = document.querySelectorAll('.volume-bar');
+}
+
+declare();
+
 function toggleAnimation() {
-  // Clone the wrapper
-  dark = !dark;
-  let clone = big_wrapper.cloneNode(true);
-  if (dark) {
-    clone.classList.remove("light");
-    clone.classList.add("dark");
-  } else {
-    clone.classList.remove("dark");
-    clone.classList.add("light");
-  }
-  clone.classList.add("copy");
-  main.appendChild(clone);
+    if (!audio.paused) {
+        pauseAudio();
+    }
 
-  document.body.classList.add("stop-scrolling");
+    // Change audio source based on theme
+    if (dark) {
+        audio.src = '/assets/audio/marvelOpening.mp3';
+    } else {
+        audio.src = '/assets/audio/endgameAudio.mp3';
+    }
 
-  clone.addEventListener("animationend", () => {
-    document.body.classList.remove("stop-scrolling");
-    big_wrapper.remove();
-    clone.classList.remove("copy");
-    // Reset Variables
-    declare();
-    events();
-  });
-}
+    // Clone the wrapper for theme animation
+    dark = !dark;
+    let clone = big_wrapper.cloneNode(true);
+    if (dark) {
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    } else {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+    document.body.classList.add("stop-scrolling");
 
-function events() {
-  toggle_btn.addEventListener("click", toggleAnimation);
-  hamburger_menu.addEventListener("click", () => {
-    big_wrapper.classList.toggle("active");
-  });
-}
-
-events();
-
-
-
-const playButton = document.querySelector('.play-button');
-const audio = document.getElementById('homePageSong1');
-let isPlaying = false;
-
-playButton.addEventListener('click', function() {
-  if (!isPlaying) {
-      audio.play();
-      playButton.src = '/assets/images/pause.png';  // Switch to pause icon
-  } else {
-      audio.pause();
-      playButton.src = '/assets/images/play.png';  // Switch back to play icon
-  }
-  isPlaying = !isPlaying;
-});
-
-
-const volumeBars = document.querySelectorAll('.volume-bar');
-
-volumeBars.forEach((bar) => {
-  bar.addEventListener('click', function() {
-    audio.volume = this.getAttribute('data-level') / (volumeBars.length - 1);
-
-    // Highlight the active bars
-    volumeBars.forEach((innerBar, index) => {
-      if (index <= this.getAttribute('data-level')) {
-        innerBar.classList.add('active');
-      } else {
-        innerBar.classList.remove('active');
-      }
+    clone.addEventListener("animationend", () => {
+        document.body.classList.remove("stop-scrolling");
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        declare();
+        bindEvents();
     });
-  });
-});
+}
+
+function playAudio() {
+    audio.play().then(() => {
+        playButton.src = '/assets/images/pause.png';
+    });
+}
+
+function pauseAudio() {
+    audio.pause();
+    playButton.src = '/assets/images/play.png';
+}
+
+function bindEvents() {
+    toggle_btn.addEventListener("click", toggleAnimation);
+    hamburger_menu.addEventListener("click", () => {
+        big_wrapper.classList.toggle("active");
+    });
+
+    playButton.addEventListener('click', function() {
+        if (audio.paused) {
+            playAudio();
+        } else {
+            pauseAudio();
+        }
+    });
+
+    volumeBars.forEach((bar, index) => {
+        bar.addEventListener('click', function() {
+            const level = parseInt(this.getAttribute('data-level'));
+            audio.volume = level / (volumeBars.length - 1);
+
+            volumeBars.forEach((innerBar, innerIndex) => {
+                if (innerIndex <= index) {
+                    innerBar.classList.add('active');
+                } else {
+                    innerBar.classList.remove('active');
+                }
+            });
+        });
+    });
+}
+
+bindEvents();
