a70573f9a794e00aca94b108437a7547b43f359d
Glendon Chin <glendonchin@gmail.com>
2023-09-26 17:17:30 -0400

    Restructered project file structure to utilize ejs, renamed files according to function, cleaned up files i.e. splitting home.css to home.css and navbar.css

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
-// Import required modules
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware to serve assets directory
-app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
+// Use ejs as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Middleware to serve static files from 'src/pages/Home' directory
-app.use(express.static(path.resolve(__dirname, 'src/pages/Home')));
+// Middleware to serve assets directory which is inside 'src' directory
+app.use('/assets', express.static(path.join(__dirname, 'src', 'assets')));
 
-// Middleware to serve static files from 'public' directory
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
-// Middleware to serve static files from 'src/pages/CharacterEncyclopedia' directory
-app.use('/CharacterEncyclopedia', express.static(path.resolve(__dirname, 'src/pages/CharacterEncyclopedia')));
-
-// Middleware to serve static files from 'src/pages/HeroMetrics' directory
-app.use('/HeroMetrics', express.static(path.resolve(__dirname, 'src/pages/HeroMetrics')));
-
-// Middleware to serve static files from 'src/pages/StoryOfTheDay' directory
-app.use('/StoryOfTheDay', express.static(path.resolve(__dirname, 'src/pages/StoryOfTheDay')));
-
-// Middleware to serve static files from 'src/pages/CharacterBookmark' directory
-app.use('/CharacterBookmarks', express.static(path.resolve(__dirname, 'src/pages/CharacterBookmarks')));
-
-
-// Middleware to serve static files from 'src' directory
-app.use(express.static(__dirname + '/src'));
+app.get('/CharacterEncyclopedia', (req, res) => {
+  res.render('characterEncyclopedia');
+});
 
 // Start the server and print the port it's running on
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
