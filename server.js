6cb68060775d521f9109d503fb122079d42fe9a8
Glendon Chin <glendonchin@gmail.com>
2023-10-07 13:45:51 -0400

    Implement logout feature

--- server.js
+++ server.js
@@ -154,7 +154,15 @@ app.post('/handleSignup', async (req, res) => {
   }
 });
 
-
+// Logout route
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+      if(err) {
+          console.log("Error : Failed to destroy the session during logout.", err);
+      }
+      res.redirect('/Auth');  // Redirect to the Auth page after logout
+  });
+});
 
 // Background job
 cron.schedule('0 0 * * *', async () => {
