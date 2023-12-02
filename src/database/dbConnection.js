0541cf7cf4493c894519ffa158a18d04f6a497bb
Glendon Chin <glendonchin@gmail.com>
2023-12-02 12:44:48 -0500

    Separate db setting for local and prod

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,15 +1,30 @@
 const mysql = require('mysql2');
-
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  socketPath: process.env.INSTANCE_UNIX_SOCKET,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: 'HeroHub',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let poolConfig;
+
+if (process.env.NODE_ENV === 'production') {
+    poolConfig = {
+        socketPath: process.env.INSTANCE_UNIX_SOCKET,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'HeroHub',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    };
+} else {
+    poolConfig = {
+        host: '34.145.188.12',
+        user: 'gchin',
+        password: 'bobthecat123',
+        database: 'HeroHub',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    };
+}
+
+const pool = mysql.createPool(poolConfig);
 
 module.exports = pool.promise();
